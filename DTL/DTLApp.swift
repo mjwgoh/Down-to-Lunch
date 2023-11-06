7717442191e162f41535e74407b53335a88cda95
Michael Goh <github@jwgoh.com>
2023-11-06 07:07:10 -0600

    Initial Commit

---
+++ DTL/DTLApp.swift
@@ -0,0 +1,17 @@
+//
+//  DTLApp.swift
+//  DTL
+//
+//  Created by Junwei Goh on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DTLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
